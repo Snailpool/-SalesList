84baadea7d1d81eb9de82bbda409e5a16317f5dc
你看看你 <Snail@nikankannide-MacBook-Pro.local>
2017-01-18 15:49:26 +0800

    try to whrite it in es6

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const FileSaver = require('file-saver');
 const fs = require('fs');
 const cssPath = 'styles.css';
 const customers = require('./customer.json'); //so convenient
+
+// return a Product object for product list.
 class Product {
 	constructor(name, pricePerBox, count, memo) {
 		this.name = name;
@@ -12,118 +14,144 @@ class Product {
 		this.memo = memo;
 	}
 }
+// handle this app
+class SalesForm {
 
-let count = 0;
-let totalPrice = 0;
-function bindCustomerInfo(input){
-	if( input.list.querySelector(`option[value='${input.value}']`) !== null){
-		const id = input.list.querySelector(`option[value='${input.value}']`).getAttribute("data-id");
-		const customer = customers[id];
-		document.getElementById('address').value = customer.address;
-		document.getElementById('phone').value = customer.phone;
-		document.getElementById('fax').value = customer.fax;
+	// bind page events
+	constructor() {
+		this.count = 0;
+		this.totalPrice = 0;
+		document.getElementById('customerName').addEventListener('change', (evt) => this.bindCustomerInfo(evt.target));
+		document.getElementById('addCustomerBtn').addEventListener('click', () => this.editCustomer());
+		document.getElementById('addProductBtn').addEventListener('click', () => this.appendProduct());
+		document.getElementById('saveFilebtn').addEventListener('click', () => this.saveFile());
 	}
-	else{
-		document.getElementById('address').value = 
-		document.getElementById('phone').value = 
-		document.getElementById('fax').value = '';
 
+	// when user choose a custoner, append other information, if it's in customers.json file.
+	bindCustomerInfo(input) {
+		if (input.list.querySelector(`option[value='${input.value}']`) !== null) {
+			const id = input.list.querySelector(`option[value='${input.value}']`).getAttribute('data-id');
+			const customer = customers[id];
+			document.getElementById('address').value = customer.address;
+			document.getElementById('phone').value = customer.phone;
+			document.getElementById('fax').value = customer.fax;
+		} else {
+			document.getElementById('address').value =
+			document.getElementById('phone').value =
+			document.getElementById('fax').value = '';
+		}
 	}
-};
-function editCustomer() {
-	document.getElementById('customerTable').innerHTML = '';
-	const fax = document.getElementById('fax').value === ''? '' : ` (傳真：${document.getElementById('fax').value})`;
-	document.getElementById('customerTable')
-			.innerHTML = `
-				<tr>
-					<td><p class="upper-block__text">列印日期：${(new Date()).toISOString().split('T')[0]}</p></td>
-				</tr>
-				<tr>
-					<td><p class="upper-block__text">客戶名稱：${document.getElementById('customerName').value}</p></td>
-				</tr>
-				<tr>
-					<td><p class="upper-block__text">客戶地址：${document.getElementById('address').value}</p></td>
-					<td><p class="upper-block__text">負責人：胡東茂</p></td>
-				</tr>
-				<tr>
-					<td><p class="upper-block__text">客戶電話：${document.getElementById('phone').value} ${fax}</p></td>
-					<td><p class="upper-block__text">聯絡電話：048950029</p></td>
-				</tr>`;
-}
-function append() {
-	count++;
-	const newProduct = new Product(document.getElementById('name').value, document.getElementById('pricePerBox').value,
-									document.getElementById('count').value, document.getElementById('memo').value);
-	document.getElementById('name').value
-		= document.getElementById('pricePerBox').value
-		= document.getElementById('count').value
-		= document.getElementById('memo').value
-		= '';
-	document.getElementById('productList')
-			.insertAdjacentHTML('beforeend', `<tr id="product${count}">
-												<td class="list-table__cell">
-													${newProduct.name}
-												</td>
-												<td class="list-table__cell">
-													${newProduct.pricePerBox}
-												</td>
-												<td class="list-table__cell">
-													${newProduct.count}
-												</td>
-												<td class="list-table__cell">
-													${newProduct.totalPrice}
-												</td>
-												<td style="width: 300px;" class="list-table__cell  list-table__cell--long">
-													${newProduct.memo}
-												</td>
-												<td  class="list-table__cell  edit-cell btn--delete" onclick="deleteProduct(${count},${newProduct.totalPrice})">
-													刪除
-												</td>
-											</tr>`);
-	totalPrice += newProduct.totalPrice;
-	document.getElementById('totalPrice').innerHTML = `總金額：${totalPrice}`;
-}
-function deleteProduct(number, priceToMinus) {
-	const domToRemove = document.getElementById(`product${number}`);
-	domToRemove.parentNode.removeChild(domToRemove);
-	totalPrice -= priceToMinus;
-	document.getElementById('totalPrice').innerHTML = `總金額：${totalPrice}`;
-}
-function saveFile(){
-	const table = adjustTableDOM().outerHTML;
-	const result = readCssFile()
-	.then((cssData) => {
-		const template = addHtmlDocument(table,cssData);
-		const converted = htmlDocx.asBlob(template);
-		FileSaver.saveAs(converted, `${document.getElementById('customerName').value}_${(new Date()).toISOString().split('T')[0]}`);
-	})
-}
-function adjustTableDOM(){
-	let copyTable = document.getElementById('salesBlock').cloneNode(true);
-	for(let i =  copyTable.getElementsByClassName('edit-cell').length; i > 0 ; i--){
-		 copyTable.getElementsByClassName('edit-cell')[0].remove();
+
+	// when user click edit customer button, show the info in the form.
+	editCustomer() {
+		document.getElementById('customerTable').innerHTML = '';
+		const fax = document.getElementById('fax').value === '' ? '' : ` (傳真：${document.getElementById('fax').value})`;
+		document.getElementById('customerTable')
+				.innerHTML = `
+					<tr>
+						<td><p class="upper-block__text">列印日期：${(new Date()).toISOString().split('T')[0]}</p></td>
+					</tr>
+					<tr>
+						<td><p class="upper-block__text">客戶名稱：${document.getElementById('customerName').value}</p></td>
+					</tr>
+					<tr>
+						<td><p class="upper-block__text">客戶地址：${document.getElementById('address').value}</p></td>
+						<td><p class="upper-block__text">負責人：胡東茂</p></td>
+					</tr>
+					<tr>
+						<td><p class="upper-block__text">客戶電話：${document.getElementById('phone').value} ${fax}</p></td>
+						<td><p class="upper-block__text">聯絡電話：048950029</p></td>
+					</tr>`;
 	}
-	return copyTable;
+
+	// when user click add product button, add one product in the list, and count total price.
+	appendProduct() {
+		this.count++;
+		const newProduct = new Product(document.getElementById('name').value, document.getElementById('pricePerBox').value,
+										document.getElementById('count').value, document.getElementById('memo').value);
+		document.getElementById('name').value
+			= document.getElementById('pricePerBox').value
+			= document.getElementById('count').value
+			= document.getElementById('memo').value
+			= '';
+		document.getElementById('productList')
+				.insertAdjacentHTML('beforeend', `<tr id="product${this.count}">
+													<td class="list-table__cell">
+														${newProduct.name}
+													</td>
+													<td class="list-table__cell">
+														${newProduct.pricePerBox}
+													</td>
+													<td class="list-table__cell">
+														${newProduct.count}
+													</td>
+													<td class="list-table__cell">
+														${newProduct.totalPrice}
+													</td>
+													<td style="width: 300px;" class="list-table__cell  list-table__cell--long">
+														${newProduct.memo}
+													</td>
+													<td  class="list-table__cell  edit-cell btn--delete" onclick="deleteProduct(${count},${newProduct.totalPrice})">
+														刪除
+													</td>
+												</tr>`);
+		this.totalPrice += newProduct.totalPrice;
+		document.getElementById('totalPrice').innerHTML = `總金額：${this.totalPrice}`;
+	}
+
+	// delete one product.
+	deleteProduct(number, priceToMinus) {
+		const domToRemove = document.getElementById(`product${number}`);
+		domToRemove.parentNode.removeChild(domToRemove);
+		totalPrice -= priceToMinus;
+		document.getElementById('totalPrice').innerHTML = `總金額：${this.totalPrice}`;
+	}
+	
+	// save this form in docx format.
+	saveFile() {
+		const table = this.adjustTableDOM().outerHTML;
+		const result = this.readCssFile()
+		.then((cssData) => {
+			const template = this.addHtmlDocument(table, cssData);
+			const converted = htmlDocx.asBlob(template);
+			FileSaver.saveAs(converted, `${document.getElementById('customerName').value}_${(new Date()).toISOString().split('T')[0]}`);
+		});
+	}
+
+	//	remove edit part of form, so that the docx file wound not show that.
+	adjustTableDOM() {
+		const copyTable = document.getElementById('salesBlock').cloneNode(true);
+		for (let i = copyTable.getElementsByClassName('edit-cell').length; i > 0; i--) {
+			copyTable.getElementsByClassName('edit-cell')[0].remove();
+		}
+		return copyTable;
+	}
+
+	//	include the style for docx file.
+	readCssFile() {
+		return new Promise((resolve, reject) => {
+			fs.readFile(cssPath, 'utf8', (err, contents) => {
+				if (err) return reject(err.message);
+				return resolve(contents);
+			});
+		});
+	}
+
+	//	combine whole structure for saving docx file.
+	addHtmlDocument(table, cssData) {
+		const cssContent = `<style>${cssData}</style>`;
+		const template = `<!DOCTYPE html>
+		<html>
+			<head>
+				<meta charset="utf-8">
+				${cssContent}
+			</head>
+		  <body>
+			${table}
+		  </body>
+		</html>`;
+		return template;
+	}
 }
-function readCssFile(){
-    return new Promise(function (resolve, reject) {
-        fs.readFile(cssPath, 'utf8', function(err, contents) {
-            if (err) return reject(err.message);
-            return resolve(contents);
-        });
-    });
-}
-function addHtmlDocument(table,cssData){
-	const cssContent = `<style>${cssData}</style>`;
-	const template = `<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="utf-8">
-			${cssContent}
-		</head>
-	  <body>
-		${table}
-	  </body>
-	</html>`;
-	return template;
-}
+const start = new SalesForm();
+
